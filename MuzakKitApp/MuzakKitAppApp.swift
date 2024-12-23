eb4069fb600d821ce142d013dc61be486a6f02bf
Damien Thompson <sedativo@gmail.com>
2024-12-22 21:11:47 -0500

    Initial Commit

---
+++ MuzakKitApp/MuzakKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuzakKitAppApp.swift
+//  MuzakKitApp
+//
+//  Created by Damien L Thompson on 2024-12-22.
+//
+
+import SwiftUI
+
+@main
+struct MuzakKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
