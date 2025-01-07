e118fabcc867a635d00bb87fc49afce2b6f14329
Damien Thompson <sedativo@gmail.com>
2025-01-06 22:01:53 -0500

    feat: add recently added section to library view

---
+++ MuzakKitApp/Screens/LibraryView.swift
@@ -0,0 +1,66 @@
+//
+//  LibraryView.swift
+//  MuzakKitApp
+//
+//  Created by Damien L Thompson on 2025-01-06.
+//
+
+import SwiftUI
+import MusicKit
+
+
+struct LibraryView: View {
+
+    @State private var items: MusicItemCollection<Album>?
+
+    var body: some View {
+
+        GeometryReader { geometry in
+
+            List {
+
+                if let items = items {
+
+                    Text("Recently Added")
+                        .font(.system(.title2))
+                        .fontWeight(.bold)
+
+                    LazyVGrid(
+                        columns: [
+                            GridItem(spacing: 12),
+                            GridItem(spacing: 12)
+                        ],alignment: .center,
+                        spacing: 24
+                    ) {
+                        ForEach(items, id: \.self) { item in
+                            AlbumItemCell(item: item, size: (geometry.size.width / 2) - 24)
+                        }
+                    }
+
+                }
+            }.listStyle(.plain)
+        }.task {
+            try? await loadRecentlyAdded()
+        }
+    }
+}
+
+extension LibraryView {
+
+    private func loadRecentlyAdded() async throws {
+
+        var request: MusicLibraryRequest<Album> = MusicLibraryRequest()
+        request.limit = 25
+        request.sort(by: \.libraryAddedDate, ascending: false)
+
+        let albumResponse = try await request.response()
+        updateSection(with: albumResponse.items)
+    }
+
+    @MainActor
+    private func updateSection(with albums: MusicItemCollection<Album>) {
+        withAnimation {
+            self.items = albums
+        }
+    }
+}
