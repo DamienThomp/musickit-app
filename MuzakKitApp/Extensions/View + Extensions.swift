16be773dff786c9aff6947a2d69babc89392400a
Damien Thompson <sedativo@gmail.com>
2025-01-08 22:00:52 -0500

    feat: add details page header styles

---
+++ MuzakKitApp/Extensions/View + Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  View + Extensions.swift
+//  MuzakKitApp
+//
+//  Created by Damien L Thompson on 2025-01-08.
+//
+
+import SwiftUI
+
+extension View {
+    
+    func plainHeaderStyle() -> some View {
+        modifier(DetailPlainSection())
+    }
+}
---
+++ MuzakKitApp/Styles/Section/DetailPlainSection.swift
@@ -0,0 +1,18 @@
+//
+//  DetailPlainSection.swift
+//  MuzakKitApp
+//
+//  Created by Damien L Thompson on 2025-01-08.
+//
+
+import SwiftUI
+
+struct DetailPlainSection: ViewModifier {
+    
+    func body(content: Content) -> some View {
+        content
+            .listStyle(.plain)
+            .listRowBackground(Color.clear)
+            .listRowSeparator(.hidden)
+    }
+}
