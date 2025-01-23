065af3819a5f4b0e3c6136e2662c9e91b88f1374
Damien Thompson <sedativo@gmail.com>
2025-01-22 22:41:28 -0500

    feat: add search to library albums page

--- MuzakKitApp/Screens/Library/AlbumLibraryScreen.swift
+++ MuzakKitApp/Screens/Library/AlbumLibraryScreen.swift
@@ -8,17 +8,104 @@
 import SwiftUI
 import MusicKit
 
+typealias SectionedALbumLibrary = [Dictionary<String.Element, [MusicItemCollection<Album>.Element]>.Element]
+
 struct AlbumLibraryScreen: View {
 
     typealias SectionedALbumLibrary = [Dictionary<String.Element, [MusicItemCollection<Album>.Element]>.Element]
 
-    @Environment(NavPath.self) private var navigation
+    @Environment(MusicKitService.self) private var musicService
+
     @State private var albums: SectionedALbumLibrary?
+    @State private var albumResults: MusicLibrarySearchResponse?
 
     @State private var searchText: String = ""
 
-    private var count: Int = 2
-    private var gutters: CGFloat = 12
+    var body: some View {
+
+        GeometryReader {
+
+            let screenWidth = $0.size.width
+
+            ScrollView {
+
+                AlbumsLibraryContainer(
+                    screenWidth: screenWidth,
+                    albums: $albums,
+                    albumResults: $albumResults
+                )
+            }
+            .frame(maxWidth: .infinity)
+            .scrollIndicators(.hidden)
+            .contentMargins([.horizontal, .top], 12)
+        }
+        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+        .onSubmit(of: .search) {
+            searchAlbums(with: searchText)
+        }
+        .navigationTitle("Albums")
+        .task { await loadAlbums() }
+
+    }
+}
+
+extension AlbumLibraryScreen {
+
+    private func searchAlbums(with query: String) {
+
+        guard !query.isEmpty else { return }
+
+        Task {
+            do {
+                let response = try await musicService.search(with: query, for: [Album.self])
+                updateSearchResults(with: response)
+            } catch {
+                print("Can't load search results for album with error: \(error.localizedDescription)")
+            }
+        }
+    }
+
+    private func loadAlbums() async {
+
+        do {
+
+            var request = MusicLibraryRequest<Album>()
+            request.sort(by: \.artistName, ascending: true)
+
+            let response = try await request.response()
+
+            let sections = Dictionary(grouping: response.items) { $0.artistName.first ?? "?" }
+            let ordered = sections.sorted( by: { $0.0 < $1.0 })
+
+            updateAlbums(with: ordered)
+        } catch {
+            print("Can't load albums with: \(error)")
+        }
+    }
+
+    @MainActor
+    private func updateAlbums(with response: SectionedALbumLibrary?) {
+        self.albums = response
+    }
+
+    @MainActor
+    private func updateSearchResults(with response: MusicLibrarySearchResponse) {
+        self.albumResults = response
+    }
+}
+
+struct AlbumsLibraryContainer: View {
+
+    @Environment(\.isSearching) private var isSearching
+    @Environment(NavPath.self) private var navigation
+
+    let screenWidth: CGFloat
+
+    @Binding var albums: SectionedALbumLibrary?
+    @Binding var albumResults: MusicLibrarySearchResponse?
+
+    let count: Int = 2
+    let gutters: CGFloat = 12
 
     private var gridColumns: [GridItem] {
         Array(repeating: .init(spacing: gutters), count: count)
@@ -28,28 +115,44 @@ struct AlbumLibraryScreen: View {
         gutters * CGFloat(count)
     }
 
+    private var cellWidth: CGFloat {
+        return (screenWidth / CGFloat(count)) - spacing
+    }
+
     var body: some View {
 
-        GeometryReader {
-
-            let cellWidth = ($0.size.width / CGFloat(count)) - spacing
-
-            ScrollView {
-
-                albumSections(cellWidth)
-            }
-            .frame(maxWidth: .infinity)
-            .scrollIndicators(.hidden)
-            .contentMargins([.horizontal, .top], gutters)
-            .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always))
+        if isSearching {
+            albumSearchResults()
+        } else {
+            albumSections()
         }
-        .navigationTitle("Albums")
-        .task { await loadAlbums() }
-
     }
 
     @ViewBuilder
-    private func albumSections(_ cellWidth: CGFloat) -> some View {
+    private func albumSearchResults() -> some View {
+
+        if let albumResults = albumResults, !albumResults.albums.isEmpty {
+
+            LazyVStack(alignment: .leading) {
+
+                LazyVGrid(
+                    columns: gridColumns,
+                    alignment: .center,
+                    spacing: spacing
+                ) {
+                    ForEach(albumResults.albums, id: \.self) { item in
+                        AlbumItemCell(item: item, size: cellWidth)
+                            .onTapGesture {
+                                navigation.path.append(AppRootScreen.DetailsView.album(item))
+                            }
+                    }
+                }.frame(maxWidth: .infinity)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func albumSections() -> some View {
 
         if let albums = albums, !albums.isEmpty {
 
@@ -77,31 +180,6 @@ struct AlbumLibraryScreen: View {
             }
         }
     }
-}
 
-extension AlbumLibraryScreen {
-
-    private func loadAlbums() async {
-
-        do {
-
-            var request = MusicLibraryRequest<Album>()
-            request.sort(by: \.artistName, ascending: true)
-
-            let response = try await request.response()
-
-            let sections = Dictionary(grouping: response.items) { $0.artistName.first ?? "?" }
-            let ordered = sections.sorted( by: { $0.0 < $1.0 })
-
-            updateAlbums(with: ordered)
-        } catch {
-            print("Can't load albums with: \(error)")
-        }
-    }
-
-    @MainActor
-    private func updateAlbums(with response: SectionedALbumLibrary?) {
-        self.albums = response
-    }
 }
 
