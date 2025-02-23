cc8fa4bb07c3e254e1bfc0637102aff6f69819b5
Damien Thompson <sedativo@gmail.com>
2025-02-23 17:01:03 -0500

    feat: add loading container view

---
+++ MuzakKitApp/Views/Common/LoadingContainerView.swift
@@ -0,0 +1,63 @@
+//
+//  LoadingContainerView.swift
+//  MuzakKitApp
+//
+//  Created by Damien L Thompson on 2025-02-23.
+//
+
+import SwiftUI
+
+enum LoadingState<T: Codable> {
+
+    case idle
+    case loading
+    case success(T)
+    case error(Error)
+}
+
+struct LoadingContainerView<T: Codable, Content: View>: View {
+
+    let loadingAction: () async throws -> T
+
+    @ViewBuilder let content: (T) -> Content
+
+    @State private var loadingState: LoadingState<T> = .idle
+
+    var body: some View {
+        VStack(alignment: .center) {
+
+            switch loadingState {
+            case .idle:
+                EmptyView()
+            case .loading:
+                ProgressView().frame(maxWidth: .infinity, maxHeight: .infinity)
+            case .success(let response):
+                content(response)
+            case .error(let error):
+                ErrorView(message: error.localizedDescription) {
+                    fetchData()
+                }
+            }
+        }.task { fetchData() }
+    }
+
+    private func fetchData() {
+
+        Task {
+            do {
+                loadingState = .loading
+                let response = try await loadingAction()
+                updateView(with: response)
+            } catch {
+                loadingState = .error(error)
+            }
+        }
+    }
+
+    @MainActor
+    private func updateView(with data: T) {
+        withAnimation {
+            loadingState = .success(data)
+        }
+    }
+}
